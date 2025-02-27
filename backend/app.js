6279618022487a9cac0d6bb1545ae8e2ead339f2
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2025-02-27 01:22:51 -0600

    [ADD] Lab11 files, pair programming with Santiago Alducin

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+//Código de pair programming con Santiago Alducin
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const plantas_routes = require("./routes/plantas2.routes");
+const other_routes = require("./routes/other.routes");
+
+app.use("/plantas", plantas_routes);
+app.use("/", other_routes);
+
+app.use((req, res, next) => {
+    res.send("ERROR: 404");
+});
+
+app.listen(3000);
